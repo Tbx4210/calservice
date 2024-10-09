7232ebbde4c8c556b116db037eb9293239452079
Athul Mathew <A00290168@mycambrian.ca>
2024-10-08 22:54:02 -0400

    calculator services

---
+++ Services/CalculatorService.cs
@@ -0,0 +1,20 @@
+﻿using calservice.Interfaces;
+
+namespace calservice.Services
+{
+    public class CalculatorService : ICalculatorServices
+    {
+        public double Add(double num1, double num2) => num1 + num2;
+
+        public double Subtract(double num1, double num2) => num1 - num2;
+
+        public double Multiply(double num1, double num2) => num1 * num2;
+
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            return num1 / num2;
+        }
+    }
+}
