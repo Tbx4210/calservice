27253adbea9dd1440a86d600a45a83462d3f37c3
Athul Mathew <A00290168@mycambrian.ca>
2024-10-08 22:43:03 -0400

    creating interface

---
+++ Interfaces/ICalculatorServices.cs
@@ -0,0 +1,10 @@
+﻿namespace calservice.Interfaces
+{
+    public interface ICalculatorServices
+    {
+        double Add(double num1, double num2);
+        double Subtract(double num1, double num2);
+        double Multiply(double num1, double num2);
+        double Divide(double num1, double num2);
+    }
+}
